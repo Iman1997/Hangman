447d224476c6c1fb405dcab61888ab9ef5d45d3e
pzudo <f.vonscholten.reg@gmail.com>
2018-08-08 13:00:25 +0200

    rest parse json object

--- app/src/main/java/com/example/bruger/hangmanv1/LoginActivity.java
+++ app/src/main/java/com/example/bruger/hangmanv1/LoginActivity.java
@@ -1,36 +1,89 @@
 package com.example.bruger.hangmanv1;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
-    EditText nameInput;
-    EditText passwordInput;
+    TextView mTextViewResult;
+
     Button login;
+
+    RequestQueue queue;
+    Gson gson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        nameInput = (EditText) findViewById(R.id.nameInput);
-        passwordInput = (EditText) findViewById(R.id.passwordInput);
-        login = (Button) findViewById(R.id.loginButton);
-        login.setOnClickListener(this);
+        mTextViewResult = (TextView) findViewById(R.id.resultTextView);
 
+        queue = Volley.newRequestQueue(this);
+        gson = new Gson();
+
+        login = (Button) findViewById(R.id.loginButton);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                jsonParse();
+            }
+        });
     }
 
+    private void jsonParse() {
+        String url= "https://api.myjson.com/bins/aakaw";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("users");
+
+                            for (int i = 0; i < jsonArray.length(); i++){
+                                JSONObject user = jsonArray.getJSONObject(i);
+
+                                String username = user.getString("username");
+                                int score = user.getInt("score");
+
+                                mTextViewResult.append(username + ", " + String.valueOf(score) + "\n\n");
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        queue.add(request);
+
+    }
 
     @Override
     public void onClick(View view) {
-        if (view.getId() == R.id.loginButton) {
-            Intent home = new Intent(LoginActivity.this, MainActivity.class);
-            LoginActivity.this.startActivity(home);
-        }
+
     }
+
 }
